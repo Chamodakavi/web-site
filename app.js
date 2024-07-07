5d3087ffee3e6410b72c08384959c2383d2be1b1
Udigithub <142435114+Chamodakavi@users.noreply.github.com>
2024-07-07 19:38:38 +0530

    nav bar v1.2 done.

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+const imgArray = [
+  {id: 1,
+    main_img: "images/2690549.jpg",
+  },
+  {id: 2,
+    main_img: "images/2690577.jpg",
+  }
+];
+
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    
+  displayMainImg();
+});
+
+const welcomeImgContainer = document.querySelector('.welcome-img-container');
+
+function displayMainImg(){
+  const firstImg = imgArray[0];
+  const main = `<img src="${firstImg.main_img}" alt="" class="welcome-img">`;
+  welcomeImgContainer.innerHTML = main;
+}
 
 const nav = document.querySelector(".nav");
-
 window.addEventListener("scroll", function () {
   const scrollHeight = window.pageYOffset;
  
@@ -11,4 +33,3 @@ window.addEventListener("scroll", function () {
   }
 
 });
-
