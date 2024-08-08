d7714116dcf074a69ba90055f2e56b06edefb35d
Udigithub <142435114+Chamodakavi@users.noreply.github.com>
2024-08-08 23:12:14 +0530

    adminDetails done.

--- admin-adminDetails.php
+++ admin-adminDetails.php
@@ -3,29 +3,48 @@
     include "function.php";
     $result = displayLoginData();
 ?>
+<?php
+    include "connection.php";
 
+    if(isset($_POST['submit'])){
+
+        $username = $_POST['username'];
+        $password1 = $_POST['password'];
+
+        $stmt = $conn->prepare("INSERT INTO logindata (username,password) VALUES (?,?)");
+        $stmt->bind_param("ss", $username, $password1);
+        
+        if($stmt->execute()){
+            // close php tag
+            header("Location: admin-contactUs.php");
+        }
+
+        
+        $stmt->close();
+        $conn->close();
+    }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin-contactUs.css">
+    <link rel="stylesheet" href="admin-adminDetails.css">
     <title>Document</title>
 </head>
 <body>
     <section>
         <div class="content-body">
-        <h1>Message Review</h1>
+        <h1>Admin Details</h1>
         <div class="line"></div>
             <div class="scroll">
                 <table>
                     <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Mobile No</th>
-                        <th>Subject</th>
-                        <th>Message</th>
+                        <th>User ID</th>
+                        <th>User Name</th>
+                        <th>Password</th>
                     </tr>
     
                     <!-- get data from db and saves it in result variable in while loop-->
@@ -53,6 +72,20 @@
                     ?>
     
                 </table>
+            </div>
+            <div class="form-container">
+                <h2>Add New User</h2>
+                <form method="post">
+                    <div class="field">
+                        <label for="username">Username :</label>
+                        <input type="text" name="username" id="username">
+                    </div>
+                    <div class="field">
+                        <label for="password">Password :</label>
+                        <input type="password" name="password" id="password">   
+                    </div>
+                    <div class="btn-field"><button type="submit" name="submit">Add</button></div>
+                </form>
             </div>
         </div>
     </section>
